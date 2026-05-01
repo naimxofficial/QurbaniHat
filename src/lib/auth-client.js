6b17faf41904e65af2f32582f83fadc988b54f0b
Naim Uddin <naimxofficial.bd@gmail.com>
2026-05-01 21:13:08 +0600

    Replaced baseURL from localhost to vercel live link

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://qurbanihat-kappa.vercel.app"
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
