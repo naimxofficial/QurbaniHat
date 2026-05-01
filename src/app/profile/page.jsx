3e7fa16af6643dacfb55fa1ee2fb98866db23e11
Naim Uddin <naimxofficial.bd@gmail.com>
2026-05-02 00:02:38 +0600

    Added name and image changing functionality

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,21 @@
 'use client'
-import { useSession } from '@/lib/auth-client';
-import { Avatar, Button } from '@heroui/react';
+import { authClient, useSession } from '@/lib/auth-client';
+import { Avatar, Button, FieldError, Form, Input, Label, Modal, TextField } from '@heroui/react';
+import { MdAccountCircle } from 'react-icons/md';
 
 
 const ProfilePage = () => {
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const image = e.target.image.value;
+
+        await authClient.updateUser({
+            name, image
+        })
+        location.reload();
+    }
 
     const { data, isPending } = useSession();
     if (isPending) {
@@ -21,7 +33,49 @@ const ProfilePage = () => {
                 </Avatar>
                 <h4 className='text-center font-bold text-xl'>Name: {user?.name}</h4>
                 <h4 className='text-center font-bold text-xl'>Email: {user?.email}</h4>
-            <Button variant='primary' className='mt-5 w-full'>Update Profile</Button>
+
+                {/* Modal */}
+                <Modal>
+                    <Button className='mt-5 w-full'>Update Information</Button>
+                    <Modal.Backdrop>
+                        <Modal.Container>
+                            <Modal.Dialog>
+                                <Modal.CloseTrigger />
+                                <Modal.Header>
+                                    <MdAccountCircle className='text-4xl' />
+                                    <Modal.Heading>Change name and image</Modal.Heading>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form className="flex w-96 flex-col gap-6 " onSubmit={onSubmit} >
+                                        <TextField
+                                            isRequired
+                                            name="name"
+                                            type="text"
+                                        >
+                                            <Label>Name</Label>
+                                            <Input placeholder="Enter your name" />
+                                            <FieldError />
+                                        </TextField>
+                                        <TextField
+                                            isRequired
+                                            name="image"
+                                            type="text"
+                                        >
+                                            <Label>Image</Label>
+                                            <Input placeholder="Enter your image URL" />
+                                            <FieldError />
+                                        </TextField>
+                                        <button
+                                            slot='close'
+                                            type='submit' className="w-full btn btn-outline btn-success rounded-full">
+                                            Update information
+                                        </button>
+                                    </Form>
+                                </Modal.Body>
+                            </Modal.Dialog>
+                        </Modal.Container>
+                    </Modal.Backdrop>
+                </Modal>
             </div>
         </div>
     );
